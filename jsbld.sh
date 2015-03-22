157496eb173bb27b6083a8e389e0fc6ee12d3689
Yuan Xulei <xyuan@mozilla.com>
2015-03-22 20:39:46 +0800

    Optimize build configuration for emscripten build.

--- jsbld.sh
+++ jsbld.sh
@@ -4,5 +4,5 @@
 ./d.sh copyphoneme
 ./d.sh
 ./d.sh -prod -mac -o2 rom cldc
-cp squawk squawk.bc && emcc squawk.bc -O3 -s TOTAL_MEMORY=167772160 --preload-file squawk.suite --pre-js pre.js -o index.html
+cp squawk squawk.bc && emcc squawk.bc -O2 -s TOTAL_MEMORY=33554432 --preload-file squawk.suite --pre-js pre.js -o index.html
 firefox index.html
--- jsbld_debug.sh
+++ jsbld_debug.sh
@@ -4,5 +4,5 @@
 ./d.sh copyphoneme
 ./d.sh
 ./d.sh -mac rom cldc
-cp squawk squawk.bc && emcc squawk.bc -s TOTAL_MEMORY=167772160 --embed-file squawk.suite --pre-js pre.js -o squawk.js
+cp squawk squawk.bc && emcc squawk.bc -O2 -s TOTAL_MEMORY=33554432 --embed-file squawk.suite --pre-js pre.js -o squawk.js
 node squawk.js
