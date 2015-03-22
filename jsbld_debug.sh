8d0f22161b8129fa6537274d95f6d8ab8d8a35b9
Yuan Xulei <xyuan@mozilla.com>
2015-03-22 19:54:09 +0800

    Fix long value aligment issue.

--- jsbld_debug.sh
+++ jsbld_debug.sh
@@ -4,5 +4,5 @@
 ./d.sh copyphoneme
 ./d.sh
 ./d.sh -mac rom cldc
-cp squawk squawk.bc && emcc squawk.bc -s TOTAL_MEMORY=167772160 --embed-file squawk.suite -o squawk.js
-node squawk.js com.sun.squawk.Test
+cp squawk squawk.bc && emcc squawk.bc -s TOTAL_MEMORY=167772160 --embed-file squawk.suite --pre-js pre.js -o squawk.js
+node squawk.js
---
+++ pre.js
@@ -0,0 +1,1 @@
+Module.arguments = ["com.sun.squawk.Test"];
