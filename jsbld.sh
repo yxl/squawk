adcdae24073e655afd3e5ececd5debccb448379b
Yuan Xulei <xyuan@mozilla.com>
2015-03-23 14:42:43 +0800

    Generate optimized js.

--- jsbld.sh
+++ jsbld.sh
@@ -4,5 +4,5 @@
 ./d.sh copyphoneme
 ./d.sh
 ./d.sh -prod -mac -gcc:emcc -o2 rom cldc
-cp squawk squawk.bc && emcc squawk.bc -O2 -s TOTAL_MEMORY=33554432 --preload-file squawk.suite --pre-js pre.js -o index.html
+cp squawk squawk.bc && emcc squawk.bc -O3 -s TOTAL_MEMORY=33554432 --preload-file squawk.suite --pre-js pre.js -o index.html
 firefox index.html
